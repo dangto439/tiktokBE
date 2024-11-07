59337b2a80f74a6683573f5be52f6d47f0a2f3c6
dangto <dang0898494156@gmail.com>
2024-11-07 09:28:56 +0700

    a

--- 1.Server/obj/Debug/net8.0/1.Server.AssemblyInfo.cs
+++ 1.Server/obj/Debug/net8.0/1.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30b760fe92d30463ea88ec637c5ad7768b9b3cd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("1.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
